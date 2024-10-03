dc3c4232c1ff14f267cb3ca5fb821e9b125c3537
unknown <hydra2medusa@gmail.com>
2024-10-03 14:38:23 +0800

    task

--- lib/Components/Profile.dart
+++ lib/Components/Profile.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:task/Components/CustomDialog.dart';
 import 'package:task/Components/Store.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class Profile extends StatefulWidget {
   final String title;
@@ -15,6 +16,114 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   bool _ontap = false;
+
+
+  //upload and show
+  Widget imageUpload(BuildContext context){
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      elevation: 1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+              width: double.infinity,
+              height: 175,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), color: Colors.white),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        child: const Text('ТӨРӨЛ СОНГОХ', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                      )
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      final ImagePicker picker = ImagePicker();
+                      final XFile? image = await picker.pickImage(source: ImageSource.camera);
+                      if (image != null) {
+                        setState(() {
+                          Store.imageCamera=File(image.path);
+                        });
+                      }
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 15),
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SvgPicture.asset('images/camera.svg'),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Text('Зураг авах'),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: 2,
+                    decoration: const BoxDecoration(
+                        color: Color(0xffC8C8C8)
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      final ImagePicker picker = ImagePicker();
+                      final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+                      if (image != null) {
+                        Store.imageGallery=File(image.path);
+                      }
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 15),
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SvgPicture.asset('images/gallery.svg'),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Text('Зургийн сангаас оруулах'),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                width: double.infinity,
+                height: 63,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.white),
+                child: const Center(
+                  child: Text(
+                    'Болих',
+                    style: TextStyle(fontSize: 14, color: Colors.red),
+                  ),
+                ),
+              )
+          ),
+        ],
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +162,7 @@ class _ProfileState extends State<Profile> {
                   showDialog(
                     context: context,
                     builder: (BuildContext context) {
-                      return const Customdialog();
+                      return imageUpload(context);
                     },
                   );
                 },
