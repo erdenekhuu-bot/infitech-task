58bf3e725c9a14204f84f8e0ffa7c7aac74c4370
unknown <hydra2medusa@gmail.com>
2024-10-03 13:15:10 +0800

    task

--- lib/pages/DriverProfile/ProfileFinished.dart
+++ lib/pages/DriverProfile/ProfileFinished.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Profilefinished extends StatefulWidget {
@@ -23,9 +24,33 @@ class _ProfilefinishedState extends State<Profilefinished> {
       body: ListView(
           padding: const EdgeInsets.all(30),
           children: [
-            SizedBox(height: MediaQuery.of(context).size.width / 5),
-            SvgPicture.asset('images/success.svg',width: 100, height: 100),
-            const Text('Таны бүртгэл амжилттай хийгдлээ', textAlign: TextAlign.center, style: const TextStyle(fontSize: 18, fontFamily: 'Roboto-Regular')),
+            SizedBox(height: MediaQuery.of(context).size.width / 9),
+            Center(
+              child: SvgPicture.asset('images/success.svg',width: 100, height: 100),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: const Text('Таны бүртгэл амжилттай хийгдлээ', textAlign: TextAlign.center, style: TextStyle(fontSize: 20, fontFamily: 'Roboto-Regular')),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                color: const Color(0xff4ADE80).withOpacity(0.5),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                   SvgPicture.asset('images/Info_Circle.svg'),
+                   Container(
+                     padding: const EdgeInsets.symmetric(vertical: 8),
+                     width: MediaQuery.of(context).size.width - 100,
+                     child: const Text('Таны бүртгэл 24-48 цагийн дотор баталгаажиж аялал хийх боломжтой болно.', style: TextStyle(fontFamily: 'Roboto-Regular', fontSize: 10, color: Color(0xff4AAF57))),
+                   )
+                ],
+              ),
+            ),
             Container(
               width: double.infinity,
               height: 52,
